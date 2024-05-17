fe11cbb5a277c1a18f6def7465563bf452abd098
irakli2206 <iraklibego1@gmail.com>
2024-05-17 10:06:29 +0400

    application instruction fix

--- app/dashboard/job/[job]/page.tsx
+++ app/dashboard/job/[job]/page.tsx
@@ -108,7 +108,7 @@ const DashboardJob = async ({ params }: Props) => {
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">რეზიუმეს გამოგზავნა</dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              გამოგზავნთ თქვენი რეზიუმე შემდეგ მისამართზე და მიუთითეთ სამსახურის დასახელება: twinit@gmail.com
+              {job.application_instruction}
             </dd>
           </div>
         </dl>
