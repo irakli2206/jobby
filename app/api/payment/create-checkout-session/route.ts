dc9f96dffd193062ac6d242d3c4e03375cdda65d
irakli2206 <iraklibego1@gmail.com>
2024-05-24 22:44:49 +0400

    .

--- app/api/payment/create-checkout-session/route.ts
+++ app/api/payment/create-checkout-session/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
- import { getProfile } from "@/app/action";
+import { getProfile } from "@/app/action";
 import { BASE_URL } from "@/env";
 
 import Stripe from "stripe"
@@ -31,7 +31,7 @@ export async function POST(
                 price: prices.data[0].id,
                 // For metered billing, do not pass quantity
                 quantity: quantity || 1,
-                
+
             },
         ],
         // metadata: {
